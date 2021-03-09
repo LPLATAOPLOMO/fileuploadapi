90d2b2dc8393bf5a2fd3d40c4ffb990ee2dc8fcc
Siddharth <siddharth2760@gmail.com>
2021-03-09 23:30:04 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const app = express();
+const port = 3000 || process.env.PORT
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'/public')))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "/uploads/"));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+app.post('/api/fileup', upload.single('upfile'), function (req, res, next) {
+    res.send(req.file)
+})
+
+app.listen(port, () => {
+    console.log('server is up ' + port);
+})
